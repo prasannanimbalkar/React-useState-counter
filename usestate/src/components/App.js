0909b8e693b60609125829e9ac11c2f6e2de2693
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-20 23:09:36 -0500

    adding

--- usestate/src/components/App.js
+++ usestate/src/components/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
-    //has [state and function]
+    //const [state, function] = useState( defaultValue );
     const [count, setCount] = useState(4);
 
     return (
